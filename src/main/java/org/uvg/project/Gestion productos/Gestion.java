d4c3b9964424b20b2fd33ac79721a11a06e19fca
Julio <julioes134@outlook.es>
2024-10-02 12:09:14 -0600

    Parte 2 de la migracion del proyecto a maven

---
+++ src/main/java/org/uvg/project/Gestion productos/Gestion.java
@@ -0,0 +1,75 @@
+package org.uvg.project.Gestion /**
+ * @author Ricardo Rodríguez
+ * @version 1
+ * Clase para simular al programador
+ */
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+class Gestion
+{   
+    private String id;
+    private String tipo;
+    private LocalDate fecha;
+    private ArrayList<String> identificadores;
+    private ArrayList<Float> cantidades;
+    private int id_empleado;
+
+    /**
+     * @param cantidad_descontada Cantidad a descontar.
+     * @param producto Producto al cual se le descontarán existencias
+    */
+
+    public Gestion(String id, String tipo, LocalDate fecha, ArrayList<Producto> productos, ArrayList<Float> cantidades, Empleado empleado)
+    {
+        this.id = id;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.cantidades = cantidades;
+        this.id_empleado = empleado.GetId();
+        ArrayList<String> id_producto = new ArrayList<>();
+        
+        for (Producto producto : productos)
+        {
+            id_producto.add(producto.GetId());
+        }
+
+        if (tipo.equals("Compras") || tipo.equals("Entradas"))
+        {
+            Entradas(cantidades, productos);
+        }
+        if (tipo.equals("Ventas") || tipo.equals("Salidas"))
+        {
+            Salidas(cantidades, productos);
+        }
+
+        this.identificadores = id_producto;
+    }
+
+    /**
+     * @param cantidad_descontada Cantidad a descontar.
+     * @param producto Producto al cual se le descontarán existencias
+    */
+
+    public final void Salidas(ArrayList<Float> cantidades, ArrayList<Producto> productos)
+    {
+        for (int i = 0; i < productos.size(); i++)
+        {
+            productos.get(i).RebajarSalida(cantidades.get(i));
+        }
+    }
+
+    /**
+     * @param cantidad_aumentar Cantidad a aumentar.
+     * @param producto Producto al cual se le aumentará existencias.
+    */
+
+    public final void Entradas(ArrayList<Float> cantidades, ArrayList<Producto> productos)
+    {
+        for (int i = 0; i < productos.size(); i++)
+        {
+            productos.get(i).AumentarExistencias(cantidades.get(i));
+        }
+    }
+}
