21ef8f287e3afbdc60700b4fd9bfb4e584189108
Julio <julioes134@outlook.es>
2024-10-16 22:08:49 -0600

    Testing objects

--- src/main/java/org/uvg/project/App.java
+++ src/main/java/org/uvg/project/App.java
@@ -1,13 +1,23 @@
 package org.uvg.project;
 
-import org.uvg.project.Security.Encryption;
 
+import org.uvg.project.GestionProductos.Producto;
+import org.uvg.project.Storage.Storage;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
+        Storage storage = Storage.loadStorage(1);
 
+        for (int i = 0; i < storage.getLocations().size(); i++) {
+            storage.getLocations().get(i).addProduct(new Producto(i, "Producto" + i, 10, "cm", 1, 1));
+        }
+
+        System.out.println("Productos en la ubicación 1:");
+        for (Producto product : storage.getLocation(1).getProducts()) {
+            System.out.println(product);
+        }
     }
 
 }
