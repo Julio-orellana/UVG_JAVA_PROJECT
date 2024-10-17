3b94a73585ef69107b1f043fcfa28512d5953ad0
Julio <julioes134@outlook.es>
2024-10-16 22:14:17 -0600

    Modificando la clase gestion para poder instanciar objetos sin problemas de compilacion

---
+++ src/main/java/org/uvg/project/GestionProductos/Gestion.java
@@ -0,0 +1,66 @@
+package org.uvg.project.GestionProductos;
+
+/**
+ * @author Ricardo Rodríguez
+ * @version 1
+ * Clase para simular al programador
+ */
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Gestion {
+    private String id;
+    private String tipo;
+    private LocalDate fecha;
+    private ArrayList<Integer> identificadores;
+    private ArrayList<Float> cantidades;
+
+    /**
+     * @param cantidad_descontada Cantidad a descontar.
+     * @param producto            Producto al cual se le descontarán existencias
+     */
+
+    public Gestion(String id, String tipo, LocalDate fecha, ArrayList<Producto> productos, ArrayList<Float> cantidades, Empleado empleado) {
+        this.id = id;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.cantidades = cantidades;
+        ArrayList<Integer> id_producto = new ArrayList<>();
+
+        for (Producto producto : productos) {
+            id_producto.add(producto.getId());
+        }
+
+        if (tipo.equals("Compras") || tipo.equals("Entradas")) {
+            Entradas(cantidades, productos);
+        }
+        if (tipo.equals("Ventas") || tipo.equals("Salidas")) {
+            Salidas(cantidades, productos);
+        }
+
+        this.identificadores = id_producto;
+    }
+
+    /**
+     * @param cantidad_descontada Cantidad a descontar.
+     * @param producto            Producto al cual se le descontarán existencias
+     */
+
+    public final void Salidas(ArrayList<Float> cantidades, ArrayList<Producto> productos) {
+        for (int i = 0; i < productos.size(); i++) {
+            productos.get(i).rebajarSalida(cantidades.get(i));
+        }
+    }
+
+    /**
+     * @param cantidad_aumentar Cantidad a aumentar.
+     * @param producto          Producto al cual se le aumentará existencias.
+     */
+
+    public final void Entradas(ArrayList<Float> cantidades, ArrayList<Producto> productos) {
+        for (int i = 0; i < productos.size(); i++) {
+            productos.get(i).aumentarExistencias(cantidades.get(i));
+        }
+    }
+}
